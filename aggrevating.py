49a6f67ad21053b6913c46c07f66b4e623a6b967
mwongeraE <evansmwongera@gmail.com>
2016-02-02 23:13:38 +0300

    mutable

---
+++ aggrevating.py
@@ -0,0 +1,14 @@
+def main():
+	x = (1,2,3) #immutable
+	print(type(x), x)
+
+if __name__ == '__main__':
+		main()
+
+		#x=[1,2,3,4] - mutable
+		#def main ():
+			#x = "string"
+			#for i in x:
+				#print (i)
+				#if __name__ == '__main__':
+				#	main()
