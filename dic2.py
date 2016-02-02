8cedeac5928fafe5d748e68466f405d6a0542c0a
mwongeraE <evansmwongera@gmail.com>
2016-02-02 23:25:30 +0300

    dictionaries

---
+++ dic2.py
@@ -0,0 +1,11 @@
+def main():
+	d = dict (
+		one = 1, two = 2, three = 3, four = 4, five = 'five'
+
+		)
+	d['seven'] = 7
+	for k in sorted(d.keys()):
+		print(k, d[k])
+
+if __name__ == '__main__':
+			main()
