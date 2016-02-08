6d61106de4c0b640c897720d1b4090d0b10e0fe8
mwongeraE <evansmwongera@gmail.com>
2016-02-08 04:03:57 +0300

    final-spell-check

---
+++ spellChecker/spellChecker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def spellcheck(sentence):
+    words = open("spell.words").readlines()
+    words = list(map(lambda x: x.strip(), words))
+    res = sentence.split()
+    ret = []
+    for item in res:
+        if(item in words):
+            continue
+        else:
+            ret.append(item)
+    return ret
+def main():
+    sentence = input("Please enter a sentence\n")
+    lst=[]
+    lst=spellcheck(sentence)
+    print(lst)
+if __name__ == "__main__": main()
